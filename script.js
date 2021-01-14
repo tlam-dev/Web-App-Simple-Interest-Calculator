57c0774af77e2081ffab4d726bca433dfff9f310
tlam-dev <65012108+tlam-dev@users.noreply.github.com>
2021-01-14 18:54:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ function compute()
     p = document.getElementById("principal").value;
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
-    result = p * (100 + r) /100;
     
+    currentY = new Date().getFullYear();
+    result = p * (100 + r) /100;
+    return result;
+    
+    console.log("If you deposit " + principal + ",");
+    console.log("at an interest rate of " + rate + "%,");
+    console.log("You will receive an amount of " + result + ",");
+    console.log("in the year " + (currentY + y) + "years."
 }
         
