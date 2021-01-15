32af1629371271ee9a73784357dedd4a41ffd880
tlam-dev <65012108+tlam-dev@users.noreply.github.com>
2021-01-15 17:29:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // JavaScript
 function compute() {
-    var p = parseFloat(document.getElementById("principal").value);
-    var r = document.getElementById("rate").value;
-    var t = document.getElementById("years").value;
+            var p = parseFloat(document.getElementById("principal").value);
+            var r = parseFloat(document.getElementById("rate").value);
+            var t = parseFloat(document.getElementById("years").value);
+            var result = (p * (1 + (r*t /100))).toFixed(2);
     
-    var y = new Date().getFullYear();
-    var result = p * (1 + (r*t /100);
-    
-    document.getElementById("output").innerHTML =
-              ("If you deposit ," + "<br/>"
-              + "at an interest rate of %," + "<br/>"
-              + "You will receive an amount of ," + "<br/>"
-              + "in the year .");}
+            var currentY = new Date().getFullYear();
+            var y = parseFloat(currentY) + t;
+            
+            if(!isNaN(p)) {
+            document.getElementById("output").innerHTML =
+              ("If you deposit " + p + "<br/>"
+              + "at an interest rate of " + r + "%," + "<br/>"
+              + "You will receive an amount of " + result + "," + "<br/>"
+              + "in the year " + y + ".");
+            } else {document.getElementById("output").innerHTML =
+              "Please enter an amount."}
+        }
